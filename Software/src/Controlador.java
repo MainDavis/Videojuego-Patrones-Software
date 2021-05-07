851ded03bb0e0fa7ae34f3fc6a1694ba52eb35cd
MainDavis <dayamon12500@gmail.com>
2021-05-07 13:12:38 +0200

    No va el boton

--- Software/src/Controlador.java
+++ Software/src/Controlador.java
@@ -1,8 +1,27 @@
 import graficos.Pantalla;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Controlador {
+    
     public static void main(String[] args) throws Exception {
+        int nivel=0;
         Pantalla pantalla = new Pantalla();
         pantalla.setVisible(true);
+
+        //Pantalla de inicio
+        pantalla.pantallaInicio();
+        
+        pantalla.btt_start.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                pantalla.cambiarSprite(); 
+            }
+        });
+ 
+        
+
     }
+
 }
+
--- Software/src/graficos/Pantalla.java
+++ Software/src/graficos/Pantalla.java
@@ -1,35 +1,97 @@
 package graficos;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Pantalla extends JFrame{
 
+    //Backgrounds
+    private ImageIcon bg_inicio = new ImageIcon("resources//backgrounds//DarkSouls_epicR.gif");
+    //Sprites
     private ImageIcon gunwoman_idle = new ImageIcon("resources//sprites//gunwoman//idle.gif");
     private ImageIcon gunwoman_attack = new ImageIcon("resources//sprites//gunwoman//attack.gif");
-    private JButton pj;
+    //GUI
+    private ImageIcon btt_inicio = new ImageIcon("resources//sprites//gui//btt_inicioR.png");
+    private JLabel pj;
+    private JLabel background;
+
+    //Botones
+    public JButton btt_start;
 
     public Pantalla(){
         super();
         configurarVentana();
-        inicializarComponentes();
+        //inicializarComponentes();
     }
 
     private void configurarVentana(){
         this.setTitle("Prueba");
-        this.setSize(600,600);
+        this.setSize(900,700);
         this.setLocationRelativeTo(null);
         this.setLayout(null);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Creo el background
+        background = new JLabel();
+        background.setBounds(0,0,900,700);
     }
 
     private void inicializarComponentes(){
-        pj = new JButton();
+        pj = new JLabel();
         pj.setIcon(gunwoman_idle);
         pj.setBounds(50,50,400,400);
-        pj.setPressedIcon(gunwoman_attack);
         this.add(pj);
     }
-}
+
+    public void pantallaInicio(){
+        
+        btt_start = new JButton();
+        btt_start.setBounds(303, 299, 294,102);
+        btt_start.setIcon(btt_inicio);
+        btt_start.setBorderPainted(false); 
+        btt_start.setContentAreaFilled(false); 
+        
+        background.setIcon(bg_inicio); 
+
+        //Ordenado de arriba a abajo
+        this.add(btt_start);
+        this.add(background);     
+
+    }
+
+    public void pantallaSeleccionDePersonaje(){
+        //Quito el boton de start
+        this.remove(btt_start);
+        
+    }
+
+    public void cambiarSprite(){
+        this.remove(btt_start);
+        this.repaint();
+    }
+
+    public void actionPerformed(ActionEvent e) { 
+        synchronized (btt_start) {
+            btt_inicio.notify();
+        }    
+    }
+
+    public static boolean waitForEnter() throws InterruptedException {
+        synchronized(btt_start) {
+            try {
+                
+                b.wait();
+            } catch (InterruptedException ex) {
+                System.out.println("Interrupted");
+            }
+        }
+        System.out.println("After button clicked");
+    //... more code
+    }
+} 
+    
